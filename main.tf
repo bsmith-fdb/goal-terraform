60c307e6352a969205b18c3b8946ac4fd9866dc2
Ben Smith <benjamin.smith@fdbhealth.com>
2022-12-18 13:43:35 -0500

    create a resource group and key vault

---
+++ main.tf
@@ -0,0 +1,42 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+        purge_soft_deleted_secrets_on_destroy = true
+        recover_soft_deleted_secrets = true
+    }
+  }
+}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "kv" {
+    name = "tf-learning-kv"
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    soft_delete_retention_days = 7
+    purge_protection_enabled = false
+    sku_name = "standard"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "tf-learning-rg"
+  location = "eastus2"
+
+  tags = {
+    Environment = "dev"
+    Team = "apollo"
+  }
+}
